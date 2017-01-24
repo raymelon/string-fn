87f6f4082bb4b7f9645d725eb32ee717941da280
self_refactor <deyan8284@gmail.com>
2017-01-24 19:57:50 +0000

    update

--- source.js
+++ source.js
@@ -1,18 +1,62 @@
 const R = require("rambda")
 
-function toKebabCase(str){
-  return R.compose(
-    val => R.replace(/[^a-zA-Z]{1,3}/g,"-",val),
-    R.join(""),
-    R.map(val =>{
-      if (R.toUpper(val) === val) {
-        return `-${ R.toLower(val) }`
-      }
+function camelToKebab(str){
 
-      return val
-    }),
-    R.split("")
-  )(str)
 }
 
-module.exports.toKebabCase = toKebabCase
+function isAlphanumeric(str){
+  return !R.test(/[^a-zA-Z0-9]/,str)
+}
+
+function isAlpha(str){
+  return !R.test(/[^a-zA-Z]/,str)
+}
+
+function isAllCaps(str){
+  return !R.test(/[^A-Z]/,str)
+}
+
+function toKebab(str){
+  if(isAlpha(str)){
+    
+    return R.compose(
+      val => {
+        if(R.head(val)==="-"){
+    
+          return R.tail(val)
+        }
+        
+        return val
+      },
+      R.join(""),
+      R.map(val=>{
+        if(R.toUpper(val)===val){
+    
+          return `-${R.toLower(val)}`
+        }
+        
+        return val
+      }),
+      R.split("")
+    )(str)
+  }
+  
+    return R.compose(
+    R.tail,
+    R.join(""),
+    R.map(val =>{
+      if (
+        val === ""
+        ) {
+       
+        return val
+      }else{
+       return `-${R.toLower(val)}`
+      }
+    }),
+    R.split(/[^a-zA-Z]{1,3}/g)
+  )(str)
+  
+}
+
+module.exports.toKebab = toKebab
--- test.js
+++ test.js
@@ -1,9 +1,31 @@
 const stringFn = require("./source")
 
-describe("toKebabCase",()=>{
-  it("should work with camel case",()=>{
+describe("toKebab", () => {
+  it("should work with camel case", () => {
     expect(
-      stringFn.toKebabCase("fooBarBaz")
+      stringFn.toKebab("fooBarBaz")
     ).toEqual("foo-bar-baz")
+  })
+
+  it("should work with snake case", () => {
+    expect(
+      stringFn.toKebab("foo_bar_baz")
+    ).toEqual("foo-bar-baz")
+  })
+
+  it("should work as lodash", () => {
+    expect(
+      stringFn.toKebab("Foo Bar BAZ")
+    ).toEqual("foo-bar-baz")    
+    
+    expect(
+      stringFn.toKebab("__FOO_BAR__")
+    ).toEqual("foo-bar")
+    
+    expect(
+      stringFn.toKebab("Foo Bar BAZ")
+    ).toEqual("foo-bar-baz")
+    
+    
   })
 })
