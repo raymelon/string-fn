4c4a37a024fb9013e948c37fa40e904b3b644308
self_refactor <deyan8284@gmail.com>
2017-01-25 00:21:32 +0000

    update

--- a.js
+++ a.js
@@ -10,4 +10,38 @@ describe("toWords", () => {
     ).toEqual(["foo", "Bar", "Baz"])
   })
 })
+
+describe("toCamelCase", () => {
+  it("should work", () => {
+    expect(
+      stringFn.toCamelCase("foo bar BAZ")
+    ).toEqual("fooBarBaz")
+  })
+  
+  it("should work with ö", () => {
+    expect(
+      stringFn.toCamelCase("foo bar bazö",true)
+    ).toEqual("fooBarBazö")
+  })
+})
+
 `
+
+let a = R.split("expect(\n")(str)
+a = R.tail(a)
+
+let willReturn = ""
+a.map(val=>{
+    if(val.includes("toEqual")){
+        let b = R.split(").toEqual(")(val)
+        let c = R.init(b[1].split("\n")[0])
+
+        willReturn +=`
+${b[0].trim()} 
+// => ${c}
+
+`        
+    }
+})
+
+console.log(willReturn)
