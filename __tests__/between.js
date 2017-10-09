d509d4576f1ed6ae70566fadd17f21aaa7ac528a
Dejan Toteff <deyan8284@gmail.com>
2017-10-09 15:26:53 +0300

    update

---
+++ __tests__/between.js
@@ -0,0 +1,15 @@
+import * as stringFn from '../stringFn'
+
+test('',()=>{
+      expect(
+        stringFn.between('begin foobarbaz end', 'foo', 'baz')
+      ).toEqual('bar')
+
+      expect(
+        stringFn.between('begin foo   bar   baz end', 'foo', 'baz')
+      ).toEqual('bar')
+
+      expect(
+        stringFn.between('begin foo bar baz end', 'q', 'x')
+      ).toEqual('begin foo bar baz end')
+})
--- __tests__/camelCase.js
+++ __tests__/camelCase.js
@@ -1,5 +1,25 @@
-import {camelCase} from '../stringFn'
+import * as stringFn from '../stringFn'
 
-test('',()=>{
-  expect(camelCase('FooBar')).toEqual('fooBar')
+test('', () => {
+  expect(
+    stringFn.camelCase('foo bar BAZ')
+  ).toEqual('fooBarBaz')
+
+  expect(
+    stringFn.camelCase('foo-bar-baz')
+  ).toEqual('fooBarBaz')
 })
+
+  test('should work as camelcase library', () => {
+    expect(
+      stringFn.camelCase('Foo-Bar')
+    ).toEqual('fooBar')
+
+    expect(
+      stringFn.camelCase('--foo.bar')
+    ).toEqual('fooBar')
+
+    expect(
+      stringFn.camelCase('Foo-Bar')
+    ).toEqual('fooBar')
+  })
