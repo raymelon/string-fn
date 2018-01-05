0542efaee9bcf5b35225bf2ecc6cef5ce02f5e90
Dejan Toteff <deyan8284@gmail.com>
2018-01-05 21:02:32 +0200

    test: fix mask sentence

--- __tests__/maskSentence.js
+++ __tests__/maskSentence.js
@@ -4,11 +4,12 @@ test('', () => {
   const sentence = 'it was, for what i need, good.'
   const expectedHidden = [ 'it', 'was', ',', 'for', 'what', 'i', 'need', ',', 'good', '.' ]
 
-  const expectedVisible = [ 'i_', 'w_s', ',', 'f_r', 'w__t', 'i', 'n__d', ',', 'g__d', '.' ]
+  const expectedVisible = [ 'it', 'w_s', ',', 'f_r', 'w__t', 'i', 'n__d', ',', 'g__d', '.' ]
 
   const { hidden, visible } = stringFn.maskSentence({ sentence })
 
   expect(hidden).toEqual(expectedHidden)
   expect(visible).toEqual(expectedVisible)
 })
+
 
