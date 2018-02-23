d2967bed3fc8c510024c992789691bea3ac5aafa
Dejan Toteff <deyan8284@gmail.com>
2018-02-23 15:38:32 +0200

    feat@important: dash and apostrophe when mask.sentence

--- __tests__/maskSentence.js
+++ __tests__/maskSentence.js
@@ -1,5 +1,27 @@
 import * as stringFn from '../src/stringFn'
 
+test('English with apostrophe(\')', () => {
+  const sentence = 'You didn\'t, do much'
+  const expectedVisible = [ 'Y_u', 'd____t', ',', 'do', 'm__h' ]
+  const expectedHidden = [ 'You', 'didn\'t', ',', 'do', 'much' ]
+
+  const { hidden, visible } = stringFn.maskSentence({ sentence })
+
+  expect(hidden).toEqual(expectedHidden)
+  expect(visible).toEqual(expectedVisible)
+})
+
+test('Bulgarian with dash(-)', () => {
+  const sentence = 'Някога е имало по-добри времена'
+  const expectedVisible = [ 'Н____а', 'е', 'и___о', 'п______и', 'в_____а' ]
+  const expectedHidden = [ 'Някога', 'е', 'имало', 'по-добри', 'времена' ]
+
+  const { hidden, visible } = stringFn.maskSentence({ sentence })
+
+  expect(hidden).toEqual(expectedHidden)
+  expect(visible).toEqual(expectedVisible)
+})
+
 test('', () => {
   const sentence = 'it was, for what i need, good.'
   const expectedHidden = [ 'it', 'was', ',', 'for', 'what', 'i', 'need', ',', 'good', '.' ]
